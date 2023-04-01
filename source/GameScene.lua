80084d885c7dc2478a23422c8b35f636c84ccec1
ohyjoseph <ohyjoseph@gmail.com>
2023-04-01 15:30:27 -0700

    Removed gemSpawner using moveWithCollisions right after it was made because it's redundant

--- source/GameScene.lua
+++ source/GameScene.lua
@@ -91,7 +91,6 @@ function initialize()
 	lavaRiseCounter = 0
 
 	gemSpawner = GemSpawner(player.y, 240)
-	gemSpawner:moveWithCollisions(0, player.y)
 
 	-- local rect = Rectangle(0, 195, 420, 150)
 	lava = Fluid(0, LAVA_STARTING_Y, 400, 70)
