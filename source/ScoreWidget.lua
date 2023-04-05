a95ea060f636afb3197c98be453cacd4b467f35f
ohyjoseph <ohyjoseph@gmail.com>
2023-04-04 21:07:08 -0700

    Made new high score flash on the widget

--- source/ScoreWidget.lua
+++ source/ScoreWidget.lua
@@ -16,6 +16,10 @@ function ScoreWidget:init(score)
 
     self.score = score
 
+    self.flashTimer = pd.frameTimer.new(60)
+	self.flashTimer.discardOnCompletion = false
+    self.flashTimer.repeats = true
+
     -- It's good practice to have these "magic numbers"
     -- be stored into a variable with a name for better
     -- readability
@@ -28,6 +32,19 @@ function ScoreWidget:init(score)
     -- Making sure this sits on top of everything else
     self:setZIndex(2000)
 
+    self:add()
+
+    self.highScoresChanged = SAVE_HIGH_SCORE(self.score)
+end
+
+function ScoreWidget:update()
+    self:drawWidget()
+    if playdate.buttonJustPressed(playdate.kButtonA) then
+        resetGame()
+    end
+end
+
+function ScoreWidget:drawWidget()
     local dialogImage = gfx.image.new(self.dialogWidth, self.dialogHeight)
     gfx.pushContext(dialogImage)
     gfx.setColor(gfx.kColorWhite)
@@ -36,16 +53,6 @@ function ScoreWidget:init(score)
     gfx.fillRoundRect(self.borderWidth, self.borderWidth, self.dialogWidth - self.borderWidth * 2,
     self.dialogHeight - self.borderWidth * 2, self.cornerRadius)
     gfx.setColor(gfx.kColorWhite)
-    -- if self.score > HIGH_SCORE then
-    local highScoresChanged = SAVE_HIGH_SCORE(self.score)
-    -- newHighScore = true
-    -- else
-    newHighScore = false
-    -- end
-    local highScoreText = "HIGH SCORE: " .. HIGH_SCORE
-    if newHighScore then
-        highScoreText = highScoreText .. " - *NEW*"
-    end
     gfx.setImageDrawMode(gfx.kDrawModeInverted)
     gfx.drawTextAligned("*High Scores*", self.dialogWidth / 2, 10, kTextAlignment.center)
     local SCORE_Y_OFFSET = 22
@@ -55,9 +62,10 @@ function ScoreWidget:init(score)
     for i = #HIGH_SCORES, 1, -1 do
         local highScore = HIGH_SCORES[i]
         local isNewHighScore = false
-        if not alreadyFoundNew and highScoresChanged and highScore == score then
+        if not alreadyFoundNew and self.highScoresChanged and highScore == self.score then
             alreadyFoundNew = true
             isNewHighScore = true
+            self.newHighScoreI = i
         end
         local scoreString
         if i == 1 then
@@ -69,6 +77,16 @@ function ScoreWidget:init(score)
         else
             scoreString = i .. "TH "
         end
+        print("hi", i, self.newHighScoreI)
+        if i == self.newHighScoreI then
+            if self.flashTimer.frame <= self.flashTimer.duration * 0.67 then
+                gfx.setImageDrawMode(gfx.kDrawModeInverted)
+            else
+                gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
+            end
+        else
+            gfx.setImageDrawMode(gfx.kDrawModeInverted)
+        end
         gfx.drawText(scoreString, self.leftPadding, SCORE_Y_OFFSET + SCORE_Y_SPACING * i)
         gfx.drawTextAligned(HIGH_SCORES[i], self.leftPadding + 89, SCORE_Y_OFFSET + SCORE_Y_SPACING * i, kTextAlignment.right)
         if isNewHighScore then
@@ -78,12 +96,4 @@ function ScoreWidget:init(score)
     gfx.drawTextAligned("_Press_ *A* _to restart_", self.dialogWidth / 2, 115, kTextAlignment.center)
     gfx.popContext()
     self:setImage(dialogImage)
-
-    self:add()
-end
-
-function ScoreWidget:update()
-    if playdate.buttonJustPressed(playdate.kButtonA) then
-        resetGame()
-    end
 end
